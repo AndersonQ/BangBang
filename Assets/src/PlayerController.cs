3b443b39f498fe571bd0c53851ed08d540273a6a
Anderson Queiroz <contato@andersonq.eti.br>
2015-06-16 20:45:32 -0300

    working

--- Assets/src/PlayerController.cs
+++ Assets/src/PlayerController.cs
@@ -3,6 +3,11 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+	public GameObject projectilePrefab;
+	public float projSpeed;
+	public float projYaw;
+	public float projPitch;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +15,16 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		GameObject proj;
+		// TODO discover how we will translate pitch and yaw to quaternions
+		// see: http://answers.unity3d.com/questions/416169/finding-pitchrollyaw-from-quaternions.html
+		Quaternion rotation = Quaternion.Euler(new Vector3(45, 45, 45));
+		if (Input.GetButton("Fire1")) {
+			proj = Instantiate(projectilePrefab, 
+			                   transform.position, 
+			                   rotation);
+
+			//proj.
+		}
 	}
 }
