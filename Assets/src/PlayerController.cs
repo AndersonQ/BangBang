0c85bd4b9879f1bb273b849f240a7aa5f841fa94
Anderson Queiroz <contato@andersonq.eti.br>
2015-06-16 22:45:26 -0300

    .

--- Assets/src/PlayerController.cs
+++ Assets/src/PlayerController.cs
@@ -18,7 +18,7 @@ public class PlayerController : MonoBehaviour {
 		GameObject proj;
 		// TODO discover how we will translate pitch and yaw to quaternions
 		// see: http://answers.unity3d.com/questions/416169/finding-pitchrollyaw-from-quaternions.html
-		Quaternion rotation = Quaternion.Euler(new Vector3(45, 45, 45));
+		Quaternion rotation = (GameObject) Quaternion.Euler(new Vector3(45, 45, 45));
 		if (Input.GetButton("Fire1")) {
 			proj = Instantiate(projectilePrefab, 
 			                   transform.position, 
