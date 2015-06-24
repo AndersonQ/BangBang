bb453b69b0762ff981faed14bddf285c7a8f1421
Makoto <makotoakamine12@gmail.com>
2015-06-23 22:36:50 -0300

    Alterado a velocidade inicial do projetil no momento da instanciacaAlterado a velocidade inicial do projetil no momento da instanciacaAlterado a velocidade inicial do projetil no momento da instanciacaooo

--- Assets/src/CylinderAngleController.cs
+++ Assets/src/CylinderAngleController.cs
@@ -3,8 +3,9 @@ using System.Collections;
 
 public class CylinderAngleController : MonoBehaviour {
 	public GameObject Projetil;
+	public GameObject Flecha;
 	CylinderAngleController Cylinder;
-
+	private bool cont = true;
 	// Use this for initialization
 	void Start () {
 		Cylinder = this;
@@ -12,15 +13,20 @@ public class CylinderAngleController : MonoBehaviour {
 			}
 	void FixedUpdate ()
 	{
-		float moveVertical = Input.GetAxis ("Vertical");
-		Cylinder.transform.eulerAngles -= new Vector3 (moveVertical, 0.0f, 0.0f);
-		if (Input.GetButton("Fire1"))
-		{
-			Object Proj =  Instantiate(Projetil,Cylinder.transform.position,Cylinder.transform.rotation);
 
-		}
+			float moveVertical = Input.GetAxis ("Vertical");
+			Cylinder.transform.eulerAngles -= new Vector3 (moveVertical, 0.0f, 0.0f);
+			if (Input.GetButton ("Fire1") && cont) {
+			Atirar(10.0f); // Trocar o valor 10 por mod da velocidade inicial aplicada.
+			//cont = false;
+			}
+
 	}
-	
+	void Atirar(float mod)
+	{
+		GameObject Proj = (GameObject)Instantiate (Projetil, Flecha.transform.position, Cylinder.transform.rotation);
+		Proj.GetComponents<Rigidbody> () [0].velocity = (Flecha.transform.TransformVector (Flecha.transform.localPosition)*mod);
+	}
 	// Update is called once per frame
 	void Update () {
 
