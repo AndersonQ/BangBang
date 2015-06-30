038ecf9dbc24865d0ddfd843ffbc24184c13176f
Makoto <makotoakamine@gmail.com>
2015-06-29 21:57:06 -0300

    Inserido o controle da velocidade inicial do projetil

--- Assets/src/CylinderAngleController.cs
+++ Assets/src/CylinderAngleController.cs
@@ -5,6 +5,7 @@ public class CylinderAngleController : MonoBehaviour {
 	public GameObject Projetil;
 	public GameObject Flecha;
 	CylinderAngleController Cylinder;
+	private float magnetud;
 	private bool cont = true;
 	// Use this for initialization
 	void Start () {
@@ -14,12 +15,22 @@ public class CylinderAngleController : MonoBehaviour {
 	void FixedUpdate ()
 	{
 
-			float moveVertical = Input.GetAxis ("Vertical");
-			Cylinder.transform.eulerAngles -= new Vector3 (moveVertical, 0.0f, 0.0f);
-			if (Input.GetButton ("Fire1") && cont) {
-			Atirar(10.0f); // Trocar o valor 10 por mod da velocidade inicial aplicada.
+		float moveVertical = Input.GetAxis ("Vertical");
+		Cylinder.transform.eulerAngles -= new Vector3 (moveVertical, 0.0f, 0.0f);
+		if (Input.GetKeyDown("space") && cont) {
+			magnetud = Time.time;
+		}
+		if (Input.GetKeyUp("space") && cont) {
+			magnetud = Time.time - magnetud;
+			Atirar(magnetud*100);
+		}
+
+
+		if (Input.GetButtonDown ("Fire1") && cont) {
+		
+			Atirar (10.0f); // Trocar o valor 10 por mod da velocidade inicial aplicada.
 			//cont = false;
-			}
+		}
 
 	}
 	void Atirar(float mod)
