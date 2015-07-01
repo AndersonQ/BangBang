d6b06a1db59a1a42422dba5dd9592ed9a2d20015
Makoto <makotoakamine12@gmail.com>
2015-06-30 22:36:55 -0300

    Ajustado variaveis e tiro

--- Assets/src/CylinderAngleController.cs
+++ Assets/src/CylinderAngleController.cs
@@ -2,41 +2,34 @@
 using System.Collections;
 
 public class CylinderAngleController : MonoBehaviour {
-	public GameObject Projetil;
-	public GameObject Flecha;
-	CylinderAngleController Cylinder;
+	public GameObject projectile;
 	private float magnetud;
-	private bool cont = true;
-	// Use this for initialization
+
 	void Start () {
-		Cylinder = this;
 
 			}
 	void FixedUpdate ()
 	{
 
 		float moveVertical = Input.GetAxis ("Vertical");
-		Cylinder.transform.eulerAngles -= new Vector3 (moveVertical, 0.0f, 0.0f);
-		if (Input.GetKeyDown("space") && cont) {
-			magnetud = Time.time;
-		}
-		if (Input.GetKeyUp("space") && cont) {
+		this.transform.eulerAngles -= new Vector3 (moveVertical, 0.0f, 0.0f);
+		if (Input.GetKeyDown("space"))
+				magnetud = Time.time;
+
+		if (Input.GetKeyUp("space")) {
 			magnetud = Time.time - magnetud;
-			Atirar(magnetud*100);
+			Atirar(magnetud*30);
 		}
 
 
-		if (Input.GetButtonDown ("Fire1") && cont) {
-		
-			Atirar (10.0f); // Trocar o valor 10 por mod da velocidade inicial aplicada.
-			//cont = false;
-		}
 
 	}
 	void Atirar(float mod)
-	{
-		GameObject Proj = (GameObject)Instantiate (Projetil, Flecha.transform.position, Cylinder.transform.rotation);
-		Proj.GetComponents<Rigidbody> () [0].velocity = (Flecha.transform.TransformVector (Flecha.transform.localPosition)*mod);
+	{ //transform.localPosition
+		GameObject Proj = (GameObject)Instantiate (projectile,transform.position + this.transform.TransformVector (new Vector3(0.0f,1.0f,0.0f)) ,Quaternion.LookRotation(this.transform.forward));
+
+		Proj.GetComponents<Rigidbody> () [0].velocity = (this.transform.TransformVector(new Vector3(0.0f,1.0f,0.0f))*mod);
+		//Proj.GetComponents<Rigidbody> () [0].velocity = (this.transform.TransformVector (this.transform.localPosition)*mod);
 	}
 	// Update is called once per frame
 	void Update () {
