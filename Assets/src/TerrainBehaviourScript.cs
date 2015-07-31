6e987512698293015b763212ba11452a93b4c52a
Makoto <makotoakamine12@gmail.com>
2015-07-31 11:53:03 -0300

    Cenario filled with mountain

--- Assets/src/TerrainBehaviourScript.cs
+++ Assets/src/TerrainBehaviourScript.cs
@@ -8,63 +8,39 @@ public class TerrainBehaviourScript : MonoBehaviour {
 	public int xBase = 0;
 	public int yBase = 0;
 	private int xResolution , zResolution;
-	private Terrain terrain;
-
 	// Use this for initialization
 	void Start () {
-		terrain = Terrain.activeTerrain;
-		terrain = this.GetComponent<Terrain>();
+
+		//Terrain terrain = this.GetComponents<Terrain>()[0];
+		Terrain terrain = Terrain.activeTerrain;
 		xResolution = terrain.terrainData.heightmapWidth;
 		zResolution = terrain.terrainData.heightmapHeight;
 		heightsMatrix = terrain.terrainData.GetHeights(0, 0, xResolution,zResolution);
-		PerlinNoiseChangeMatrix ();
 
-
-
-		//ResetHeightMatrix ();
-		terrain.terrainData.SetHeights(0, 0, heightsMatrix);
-	}
-
-		// Update is called once per frame
-	void Update () {
 		/*
-		ResetHeightMatrix ();
-		terrain.terrainData.SetHeights(0, 0, heightsMatrix);
-		PerlinNoiseChangeMatrix ();
-		terrain.terrainData.SetHeights(0, 0, heightsMatrix);
+		this.terrain.terrainData.
+		this.terrain.terrainData.SetHeights(0, 0, this.originalHeights);
 		*/
-
-	}
-
-	private void ResetHeightMatrix()
-	{
 		int i, j = 0;
 		i = 0;
-		
+		//for (i=0; i<terrain.terrainData.heightmapHeight/10; i++) {
+		//	for(j=0;j<terrain.terrainData.heightmapWidth/10;j++)
 		for (i=0; i<xResolution; i++) {
 			for(j=0;j<zResolution;j++)
 			{
-				heightsMatrix[i,j] = 0;
+				heightsMatrix[i,j] = Mathf.Cos(i) - Mathf.Cos(j);
+				//originalHeights[30,30] = 100;
+
 			}
 		}
-		
+
+		terrain.terrainData.SetHeights(0, 0, heightsMatrix);
+
 	}
 
-	private void PerlinNoiseChangeMatrix()
-	{
-		int i, j = 0;
-		i = 0;
-		float scale = terrain.terrainData.heightmapHeight;
-		float timeFactor = Time.time*1;
-		float simpleFactor = 10f;
-		timeFactor = 1f;
-		//simpleFactor = 1f;
-		for (i=0; i<xResolution; i++) {
-			for(j=0;j<zResolution;j++)
-			{
-				heightsMatrix[i,j] = Mathf.PerlinNoise((simpleFactor*timeFactor*i)/scale,(simpleFactor*timeFactor*j)/scale);
-			}
-		}
 
+	// Update is called once per frame
+	void Update () {
+	
 	}
 }
